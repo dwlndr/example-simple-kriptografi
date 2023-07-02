fe8a77d4c8b688a03b9c4724e06ea2421ff76cc3
Ardhilla Eka <86395605+dwlndr@users.noreply.github.com>
2023-07-02 14:00:56 +0700

    Add files via upload

---
+++ caesarcipher/akhir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="dekCaesar.php" method="get">
+        Key : <input type="text" name="key" maxlength="2"><br>
+        <input type="submit" value="kirim">
+        <input type="reset" value="ulangi">
+    </form>
+</body>
+</html>
