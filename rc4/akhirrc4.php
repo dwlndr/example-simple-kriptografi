f27d32869b048a6394b550efd6db1a6f3b9021ba
Ardhilla Eka <86395605+dwlndr@users.noreply.github.com>
2023-07-02 14:03:29 +0700

    Add files via upload
    rc4

---
+++ rc4/akhirrc4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="pedekripsi.php" method="get">
+        Key : <input type="text" name="kcdekripsi" maxlength="16"><br>
+        <input type="submit" value="kirim">
+        <input type="reset" value="ulangi">
+    </form>
+</body>
+</html>
---
+++ rc4/awalrc4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="penkripsi.php" method="get">
+        Plainteks : <input type="text" name="kalimat"/><br>
+        Key : <input type="text" name="kcenkripsi" maxlength="16"><br>
+        <input type="submit" value="kirim">
+        <input type="reset" value="ulangi">
+    </form>
+</body>
+</html>
